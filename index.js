10ca4e540e132e8e85768a5d21d830585313de5e
Rony163 <minhajulislamrony43@gmail.com>
2021-10-28 03:27:11 +0600

    connect to the database

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+// Middlewire
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gltfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db('bd-volunteer');
+        const volunteerCollection = database.collection('volunteer');
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Volunteer server runnig');
-})
+    res.send('Running Volunteer Server');
+});
 
-app.listen(port, (req, res) => {
-    console.log('Server running on port :', port);
-})
+app.listen(port, () => {
+    console.log('Server running on port:', port);
+});
